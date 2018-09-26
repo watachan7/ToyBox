88e7a96aaacb515f3ee9b4df2f9ce22eaa6e62d6
watachan7 <watarund7@gmail.com>
2018-09-26 12:00:19 +0900

    I want to delete this code!!

--- Java_works/ArrayDataBuilder(SphereTheAI).java
+++ Java_works/ArrayDataBuilder(SphereTheAI).java
@@ -8,51 +8,87 @@ import java.io.InputStreamReader;
 public class ArrayDataBuilder {
 	
 	/**
-	 * 「文字列を検索する」送られてきた文字を1文字ずつ検証し、配列に格納して返す
+	 * 文字列を検索する
+	 * 送られてきた文字を1文字ずつ検証し、配列に格納して返す。
 	 */
 	public String[] searchAndMake(String[] item, String[] message) {
+
 		String[] itemBox = new String[1000];
 		String chank = "";
+
 		for (int i = 0; i < message.length; i++) {
-			if (message[i] == null) {message[i] = "";}
+			if (message[i] == null) {
+				message[i] = "";
+			}
+
 			chank += message[i];
+
 			for (int j = 0; j < item.length; j++) {
 				if (chank.equals(item[j])) {
-					for (int k = 0; k < itemBox.length; k++) {if (chank.equals(item[k])) {itemBox[k] = chank;}}
+					for (int k = 0; k < itemBox.length; k++) {
+						if (chank.equals(item[k])) {
+							itemBox[k] = chank;
+						}
+					}
 				}
 			}
-		}return itemBox;
+
+		}
+
+		return itemBox;
 	}
 	
 	/**
-	 * 「文字列をミンチにする」受け取った文字列をchar単位に分解して配列に格納して返す
+	 * 文字列をミンチにする
+	 * 受け取った文字列をchar単位に分解して配列に格納して返す
 	 */
 	public String[] messageReorganization(String message) {
+
 		String[] chank = new String[1000];
+
 		for (int i = 0; i < message.length(); i++) {
 			chank[i] = String.valueOf(message.charAt(i));
-		}return chank;
+		}
+
+		return chank;
 	}
 	
 	/**
-	 * 「ファイル内容を配列に入れる」txtファイルをロードし、内容を配列に格納する
+	 * ファイル内容を配列に入れる
+	 * txtファイルをロードし、内容を配列に格納する
 	 */
 	public String[] loadAndCreate(String fileName) {
+
 		BufferedReader bf = null;
 		String[] inputItem = new String[1000];
+
 		try {
 			InputStream is = getClass().getClassLoader().getResourceAsStream(fileName);
 			bf = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+
 			String str = null;
-			for (int i = 0; (str = bf.readLine()) != null; i++) {inputItem[i] = str;}
+			for (int i = 0; (str = bf.readLine()) != null; i++) {
+				inputItem[i] = str;
+			}
+
 			for (int i = 0; i < inputItem.length; i++) {
-				if (inputItem[i] == null) {inputItem[i] = "";}
-			}bf.close();}
-		catch (FileNotFoundException e) {return null;} 
-		catch (NullPointerException e) {return null;} 
-		catch (IOException e) {return null;}
+				if (inputItem[i] == null) {
+					inputItem[i] = "";
+				}
+			}
+			bf.close();
+
+		} catch (FileNotFoundException e) {
+			return null;
+		} catch (NullPointerException e) {
+			return null;
+		} catch (IOException e) {
+			return null;
+		}
+
 		return inputItem;
 	}
+
 	/**
 	 * ver2(指定ファイルの内容が決まっている時)
 	 */
@@ -72,23 +108,30 @@ public class ArrayDataBuilder {
 		catch (IOException e) {return null;}
 		return inputItem;
 	}
+
 	// ファイルをロードして配列に返還
 	public String[] loadAndCreate3(String addless){
 		int line = LineGetters(addless);
 		String[] Box = loadAndCreate2(addless,line);return Box;
 	}
+
 	// ファイルをロードしてその行数を取得
 	public int LineGetters(String fileName){
 		BufferedReader bf = null;
 		int count = 0;
+
 		try {
 			InputStream is = getClass().getClassLoader().getResourceAsStream(fileName);
 			bf = new BufferedReader(new InputStreamReader(is, "UTF-8"));
 			String str = null;
 			for (int i = 0; (str = bf.readLine()) != null; i++) {
 				count = count + 1;
-			}bf.close();}
-		catch (Exception e) {}
+			}
+			bf.close();
+
+		} catch (Exception e) {
+			return null;
+		}
 		return count;
 	}
 
@@ -98,13 +141,19 @@ public class ArrayDataBuilder {
 	public String[] searchSubject(String explain) {
 		String[] subject = loadAndCreate("Subject.txt");
 		String[] subBox = new String[1000];
-		for (int i = 0; i < subject.length; i++) {
-			if (explain.matches(".*" + subject[i] + ".*")) {subBox[i] = subject[i];}
-		}return subBox;
+
+		for (int i = 0; i < subject.length; i++) {			
+			if (explain.matches(".*" + subject[i] + ".*")) {
+				subBox[i] = subject[i];
+			}
+		}
+
+		return subBox;
 	}
 	
 	public void p(String str) {
 		System.out.println(str);
 		System.out.println();
+
 	}
 }
