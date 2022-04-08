409ad987aa146cc4d19ae40ecfa2ad3c78b97f9a
watarund7 <watarund7@gmail.com>
2022-04-09 00:32:47 +0900

    Souorce Fix

--- Python_works/pyQt/ViewWindows.py
+++ Python_works/pyQt/ViewWindows.py
@@ -1,16 +1,23 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 import sys
-from PyQt5 import QtGui
-
-def main():
-    app = QtGui.QApplication(sys.argv)
-    w = QtGui.QWidget()
-    w.resize(250, 150)
-    w.setWindowTitle('QtSample')
-    #w.setWindowIcon(QtGui.QIcon('pythonlogo.png'))  #アプリケーションアイコンを設定 (Pythonのロゴ)
-    w.show()
-    sys.exit(app.exec_())
-
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
+from PyQt5.QtGui import QIcon
+ 
+class MainWindow(QWidget):
+    def __init__(self, parent=None):
+        super(MainWindow, self).__init__(parent) # 初期化
+        self.initUI() # UIの初期化
+    def initUI(self): # UIの初期化をするメソッド
+        self.resize(400, 300) # ウィンドウの大きさの設定(横幅, 縦幅)
+        self.move(400, 300) # ウィンドウを表示する場所の設定(横, 縦)
+        self.setWindowTitle('PyQt5 sample GUI') # ウィンドウのタイトルの設定
+        btn = QPushButton('Hello World PyQt5', self) # ボタンウィジェット作成
+        btn.resize(btn.sizeHint()) # ボタンのサイズの自動設定
+        btn.move(100, 50) # ボタンの位置設定(ボタンの左上の座標)
+ 
 if __name__ == '__main__':
-    main()
+    app = QApplication(sys.argv) #PyQtで必ず呼び出す必要のあるオブジェクト
+    main_window = MainWindow() #ウィンドウクラスのオブジェクト生成
+    main_window.show() #ウィンドウの表示
+    sys.exit(app.exec_()) #プログラム終了
